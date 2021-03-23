92cf246d6ace853430a3d89282e37cd9a6108c2a
yevge <yevgeniy@wang>
2021-03-23 21:31:05 +0200

    User Agent Service 2

---
+++ src/DonatjParserService.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace Hillel\UserAgent;
+
+
+use donatj\UserAgent\UserAgentParser;
+use Hillel\UserAgentInterface\UserAgentServiceInterface;
+
+class DonatjParserService implements UserAgentServiceInterface
+{
+    protected $data;
+    protected $parser;
+
+    public function __construct()
+    {
+        $this->parser = new UserAgentParser();
+    }
+
+    public function parse($ua)
+    {
+        $this->data = $this->parser->parse($ua);
+    }
+
+    public function browser()
+    {
+        return $this->data->browser();
+    }
+
+    public function os()
+    {
+        return $this->data->platform();
+    }
+}
